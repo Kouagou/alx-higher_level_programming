a3676eeac58164a33576e15b4967f653e981a698
Kouagou Kenneth <ntchakenneth@gmail.com>
2023-11-29 15:10:18 -0800

    Task 10. Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,7 +9,7 @@
  *
  * Return: 0 if there is no cycle, 1 if there is a cycle
  */
-int check_cycle(listint_t *list)
+/*int check_cycle(listint_t *list)
 {
 	listint_t *slow, *fast;
 
@@ -19,6 +19,28 @@ int check_cycle(listint_t *list)
 	slow = list;
 	fast = slow->next;
 	while (fast != NULL && fast->next != NULL)
+	{
+		if (slow == fast)
+			return (1);
+
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+	return (0);
+}*/
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	slow = list->next;
+	fast = list->next->next;
+
+	while (slow && fast && fast->next)
 	{
 		if (slow == fast)
 			return (1);
