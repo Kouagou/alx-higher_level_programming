894a7bc47a812c1cd89dff01172e36448c241a26
Kouagou Kenneth <ntchakenneth@gmail.com>
2023-12-04 15:37:55 -0800

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv) -1 == 1:
-    print('{:d} argument:'.format(len(sys.argv) -1))
-elif len(sys.argv) -1 == 0:
-    print('{:d} arguments.'.format(len(sys.argv) -1))
-else:
-    print('{:d} arguments:'.format(len(sys.argv) -1))
+if __name__ == "__main__":
+    import sys
 
-for i in range(1, len(sys.argv)):
-    print('{:d}: {}'.format(i, sys.argv[i]))
+    if len(sys.argv) - 1 == 1:
+        print('{:d} argument:'.format(len(sys.argv) - 1))
+    elif len(sys.argv) - 1 == 0:
+        print('{:d} arguments.'.format(len(sys.argv) - 1))
+    else:
+        print('{:d} arguments:'.format(len(sys.argv) - 1))
+    for i in range(1, len(sys.argv)):
+        print('{:d}: {}'.format(i, sys.argv[i]))
