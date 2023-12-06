489e353a40716f94613e7d09e9be90933414f084
Kouagou Kenneth <ntchakenneth@gmail.com>
2023-12-06 14:27:29 -0800

    Task 0. Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    return [list(map(lambda x: x**3, row)) for row in matrix]
+    if matrix:
+        return [list(map(lambda x: x**3, row)) for row in matrix]
+    return matrix
