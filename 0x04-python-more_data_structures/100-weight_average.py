002bfed9e4e22902d040ecabddf2eb8dbd2fbfe2
Kouagou Kenneth <ntchakenneth@gmail.com>
2023-12-07 16:06:03 -0800

    Task 13. Weighted average!

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list:
+        num = 0
+        denom = 0
+        for (x, y) in my_list:
+            num += x *y
+            denom += y
+        return num / denom
+    else:
+        return 0
