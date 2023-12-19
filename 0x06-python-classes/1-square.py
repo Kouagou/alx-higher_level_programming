b3cd4cdae4f096d5d56b71c59c6850e4f9c4c24a
Kouagou Kenneth <ntchakenneth@gmail.com>
2023-12-19 15:19:29 -0800

    Task 1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 0-square.py by Kenneth K. N'TCHA
+"""Defines a square """
+
+
+class Square:
+    """Does anything"""
+
+    def __init__(self, size):
+        """Instantiation with size (no type/value verification)
+        Args:
+            size - represent the size of the square
+        """
+
+        self.__size = size
