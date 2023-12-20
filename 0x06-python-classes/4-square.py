fe60e8d1b663687a4b2567e516d0b436051cccb4
Kouagou Kenneth <ntchakenneth@gmail.com>
2023-12-19 16:20:04 -0800

    Task 4. Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+# 0-square.py by Kenneth K. N'TCHA
+"""Defines a square """
+
+
+class Square:
+    """A class Square that defines a square"""
+
+    def __init__(self, size=0):
+        """Instantiation with size (no type/value verification)
+        Args:
+            size - represent the size of the square
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Calculate the square area
+        Returns:
+            the current square area
+        """
+
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """Retrieve the size value"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set size value
+        Args:
+            value - the new value to be set
+        """
+
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
