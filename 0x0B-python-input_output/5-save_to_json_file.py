7c4536cacefbf8b197217da3dc395c77c95b30b6
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-01-09 15:25:48 -0800

    Task 6. Create object from a JSON file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,4 +7,4 @@ def save_to_json_file(my_obj, filename):
     """ A function that writes an Object to a text file,
     using a JSON representation. """
     with open(filename, 'w', encoding='utf-8') as f:
-        f.write(json.dumps(my_obj))
+        json.dump(my_obj, f)
