8c45338248fa8141fd1aafbe4613fee21b0754c6
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-01-09 09:58:59 -0800

    Task 0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+""" Defines a text file reading function """
+
 
 def read_file(filename=""):
+    """ Read a text file and prints it to stdout. """
     with open(filename, encoding="utf-8") as f:
-        for line in f:
-            print(line, end="")
+        print(f.read(), end="")
