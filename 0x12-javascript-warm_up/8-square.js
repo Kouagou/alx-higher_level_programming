ce22585ff099ff4d5c11b18e4a710a2bcf770c1a
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-03-11 18:38:05 -0700

    Task 8. Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 const num = Number(process.argv[2]);
-for (let i = 0; i < num; i++) {
-  let str = '';
-  for (let j = 0; j < num; j++) {
-    str += 'X';
+if (num) {
+  for (let i = 0; i < num; i++) {
+    let str = '';
+    for (let j = 0; j < num; j++) {
+      str += 'X';
+    }
+    console.log(str);
   }
-  console.log(str);
+} else {
+  console.log('Missing size');
 }
