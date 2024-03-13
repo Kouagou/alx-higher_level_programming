53efce14d106c5da02e65d955741cbacf037e59e
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-03-12 18:59:58 -0700

    Task 6. Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) {
+        let str = '';
+        for (let j = 0; j < this.width; j++) {
+          str += c.toString();
+        }
+        console.log(str);
+      }
+    } else {
+      super.print();
+    }
+  }
+}
+module.exports = Square;
