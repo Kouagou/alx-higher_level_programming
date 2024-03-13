2f4f1f6ebe2ea851d41a19bd28d5484be3da3f8f
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-03-12 19:05:59 -0700

    Task 6. Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -11,7 +11,7 @@ class Square extends Rectangle {
       for (let i = 0; i < this.height; i++) {
         let str = '';
         for (let j = 0; j < this.width; j++) {
-          str += c.toString();
+          str += c;
         }
         console.log(str);
       }
