4a9ec5bac9067234df86d453a19122ef0f598fab
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-03-20 04:02:18 -0700

    Task 5. All cities by state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" A script that takes in the name of a state as an argument and
+    lists all cities of that state, using the database hbtn_0e_4_usa
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """ Create a connexion and then fetch all the cities
+        by state.
+    """
+    cnx = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                          passwd=argv[2], db=argv[3], charset="utf8")
+    cur = cnx.cursor()
+    cur.execute("SELECT cities.name FROM \
+            cities INNER JOIN states ON cities.state_id = states.id \
+            WHERE states.name = %s ORDER BY cities.id ASC", (argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        for col in row:
+            print("{}".format(col))
+    cur.close
+    cnx.close()
