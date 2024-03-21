0b1f3270f512f9c3e20e46522d9ebd204de70f44
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-03-21 16:10:07 -0700

    Task 16. List relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -21,6 +21,6 @@ if __name__ == "__main__":
             print("{}: {}".format(state.id, state.name))
             if state.cities is not None:
                 for city in state.cities:
-                    print("\t{}: {}".format(city.id, city.name))
+                    print("    {}: {}".format(city.id, city.name))
 
     session.close()
