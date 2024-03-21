bf5af84c956594569cd1906a30814d8200029cc2
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-03-21 16:04:11 -0700

    Task 16. List relationship

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" A script that lists all State objects, and corresponding City objects,
+    contained in the database hbtn_0e_101_usa.
+"""
+from relationship_state import Base, State
+from sqlalchemy import create_engine
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from relationship_city import City
+from sqlalchemy.orm import lazyload
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                           argv[2], argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).options(lazyload(State.cities)).all()
+    if states is not None:
+        for state in states:
+            print("{}: {}".format(state.id, state.name))
+            if state.cities is not None:
+                for city in state.cities:
+                    print("\t{}: {}".format(city.id, city.name))
+
+    session.close()
