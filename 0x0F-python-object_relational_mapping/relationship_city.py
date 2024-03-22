e377a53bda74b2c1fd4ff2de6c1a3d46bc7f7477
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-03-22 03:35:55 -0700

    Task 16. List relationship

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-""" A  Python file similar to model_state.py named model_city.py
-    that contains the class definition of a City.
 """
-from sqlalchemy import Column, Integer, String, ForeignKey
+This script defines a City class
+to work with MySQLAlchemy ORM.
+"""
+
 from relationship_state import Base, State
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class City(Base):
-    """ Defines City class """
-
+    """City class
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The id of the class
+        name (str): The name of the class
+        state_id (int): The state the city belongs to
+    """
     __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-
-    state = relationship("State", back_populates="cities")
