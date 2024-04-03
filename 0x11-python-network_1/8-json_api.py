f5643aacaef1650479a625809e78480ae9d4ed9f
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-04-03 05:14:43 -0700

    Task 8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,7 +8,7 @@ import requests
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    letter = "" if len(argv) == 1 else sys.argv[1]
+    letter = "" if len(argv) == 1 else argv[1]
     res = requests.post(url, data={"q": letter})
     try:
         body = res.json()
