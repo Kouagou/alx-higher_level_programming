4a1f8ee186ac7c28a693a79f18ab49b4bb8dd85c
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-04-03 05:11:33 -0700

    Task 8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,9 +8,8 @@ import requests
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    # url = "http://c3105be4503c.b063733e.alx-cod.online:5000/search_user"
-    q = "" if len(argv) == 1 else sys.argv[1]
-    res = requests.post(url, data={'q': q})
+    letter = "" if len(argv) == 1 else sys.argv[1]
+    res = requests.post(url, data={"q": letter})
     try:
         body = res.json()
         if body == {}:
