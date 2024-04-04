0c282d9441a596cdf79b848b437d3943229d75ce
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-04-04 05:12:27 -0700

    Task 6. Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Define a function that find a peak. """
+
+
+def find_peak(list_of_integers):
+    """
+        Args:
+            list_of_integers(int): list of integers to find peak of
+        Returns: peak of list_of_integers or None
+    """
+    if len(list_of_integers) == 0:
+        return None
+    elif len(list_of_integers) == 1:
+        return list_of_integers[0]
+    else:
+        peak = 0
+        for i in range(len(list_of_integers)):
+            if i == 0:
+                if list_of_integers[i] >= list_of_integers[i + 1]:
+                    peak = list_of_integers[i]
+            elif i == (len(list_of_integers) - 1):
+                if list_of_integers[i] >= list_of_integers[i - 1]:
+                    peak = list_of_integers[i]
+            else:
+                if list_of_integers[i] >= list_of_integers[i - 1] and list_of_integers[i] >= list_of_integers[i + 1]:
+                    peak = list_of_integers[i]
+        return peak
