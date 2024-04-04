50aac3a70094a98e66a07e994d4ec98d5f1be8a2
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-04-04 06:30:58 -0700

    Task 6. Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -19,8 +19,9 @@ def find_peak(list_of_integers):
     while start <= end:
         mid = (start + end) // 2
 
-        if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1]) and \
-           (mid == size - 1 or list_of_integers[mid] >= list_of_integers[mid + 1]):
+        if ((mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1])
+            and (mid == size - 1 or
+                 list_of_integers[mid] >= list_of_integers[mid + 1])):
             return list_of_integers[mid]
         elif mid > 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
             end = mid - 1
