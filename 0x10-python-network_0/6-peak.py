b19dd1cd71e99f6d24c9a9583e6846bb4c3732e0
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-04-04 06:24:52 -0700

    Task 6. Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,21 +8,23 @@ def find_peak(list_of_integers):
             list_of_integers(int): list of integers to find peak of
         Returns: peak of list_of_integers or None
     """
-    if len(list_of_integers) == 0:
+    size = len(list_of_integers)
+
+    start = 0
+    end = size - 1
+
+    if size == 0:
         return None
-    elif len(list_of_integers) == 1:
-        return list_of_integers[0]
-    else:
-        peak = 0
-        for i in range(len(list_of_integers)):
-            if i == 0:
-                if list_of_integers[i] >= list_of_integers[i + 1]:
-                    peak = list_of_integers[i]
-            elif i == (len(list_of_integers) - 1):
-                if list_of_integers[i] >= list_of_integers[i - 1]:
-                    peak = list_of_integers[i]
-            else:
-                if (list_of_integers[i] >= list_of_integers[i - 1] and
-                        list_of_integers[i] >= list_of_integers[i + 1]):
-                    peak = list_of_integers[i]
-        return peak
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1]) and \
+           (mid == size - 1 or list_of_integers[mid] >= list_of_integers[mid + 1]):
+            return list_of_integers[mid]
+        elif mid > 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return None
