fd2b379d8d6d16cdcb8115aa206a8df595dc41e8
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-04-24 08:16:43 -0700

    Task 6. How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,23 +5,18 @@ const argv = process.argv;
 request(argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
-  } else {
-    const todos = JSON.parse(body);
-    const results = {};
-    const users = new Set();
-    for (const todo of todos) {
-      users.add(todo.userId);
-    }
-
-    for (const user of users) {
-      let count = 0;
-      for (const todo of todos) {
-        if (user === todo.userId && todo.completed === true) {
-          count++;
-        }
-      }
-      results[user] = count;
-    }
-    console.log(results);
+	  return;
   }
+  const todos = JSON.parse(body);
+  const results = {};
+  todos.forEach((todo) => {
+    if (todo.completed) {
+      if (!results[todo.userId]) {
+        results[todo.userId] = 1;
+      } else {
+        results[todo.userId] += 1;
+      }
+    }
+  });
+  console.log(results);
 });
