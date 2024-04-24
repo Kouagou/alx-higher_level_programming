4b300e016cdc20cfaae512f9f92b097e4ae5c38b
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-04-24 01:54:59 -0700

    Task 0. Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,6 +3,10 @@ const fs = require('fs');
 const argv = process.argv;
 
 fs.readFile(argv[2], 'utf8', function (err, data) {
-  const content = data;
-  console.log(content);
+  if (err) {
+    console.log(err);
+  }
+  else {
+    console.log(data);
+  }
 });
